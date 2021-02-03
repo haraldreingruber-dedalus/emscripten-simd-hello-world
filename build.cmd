492bf3adeb4579807c8508cd5775a43d82a47b8a
haraldreingruber-dedalus <74898239+haraldreingruber-dedalus@users.noreply.github.com>
2021-02-03 17:50:42 +0100

    Add comment

--- build.cmd
+++ build.cmd
@@ -1,3 +1,5 @@
+REM Checkout the build-debug.cmd in case you want to verify the SIMD instructions in the WASM disassembly. 
+REM In the release version the const computation is optimized quite a bit :)
 em++ main.cpp ^
   -std=c++20 ^
   -s WASM=1 ^
